2a5135d7571b79d54e6ad150b8e29c59566daca5
Everett Smith <evepsmith15@gmail.com>
2019-08-02 13:07:10 -0700

    adding css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,12 @@ function Profile () {
         <div className="profile">
         <h1>Profile</h1>
         <p></p>
-        <h1></h1>
+        <h1>Info</h1>
+        <p>email: </p>
+        <p>Favorite food: </p>
+        <p>Favorite drink: </p>
+        <p>Allergies</p>
+        <p>Favorite Location</p>
         </div>
         );
 }
