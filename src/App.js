f0dc42de1ee30e64028c1a5b87c5bf780f00f9f7
Jacob Zamano <zamano.jacob@gmail.com>
2019-08-03 23:16:16 -0700

    added react routing, added login page + register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home.js";
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
 
 function App() {
   return (
-<>
-<Home/>
-</>
+  <Router>
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/register" component={Register} />
+    </Switch>
+  </Router>
   );
 }
 
