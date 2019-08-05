0eb2e47f32eafce5bd0f92bbb629e6409d862cb2
Everett Smith <evepsmith15@gmail.com>
2019-08-05 11:29:30 -0700

    adding

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -10,9 +10,13 @@ class Profile extends Component {
         <h1>Info</h1>
         <p>email: </p>
         <p>Favorite food: </p>
+        <input type="text" class="form-control" name="FavFood" id="FavFood"/> 
         <p>Favorite drink: </p>
+        <input type="text" class="form-control" name="FavDrink" id="FavDrink"/> 
         <p>Allergies</p>
+        <input type="text" class="form-control" name="Allergies" id="Allergies"/> 
         <p>Favorite Location</p>
+        <input type="text" class="form-control" name="FavLocation" id="FavLocation"/> 
         </div>
         );
     }
