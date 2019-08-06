53e75afc2b3908bdb4afc346a39a68c3b5248a6f
Jacob Zamano <zamano.jacob@gmail.com>
2019-08-06 09:58:54 -0700

    added state and variable changes on forms + added handle form submit for values to be sent to the backend

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,35 +1,66 @@
 import React, { Component } from "react";
-import "./login.css"
+import "./login.css";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
   componentDidMount() {
     document.body.classList.add("background-login");
   }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    alert(`Hello ${this.state.email}`);
+  };
+
   render() {
     return (
       <div className="container">
-      <div className="align-items-center">
-          <h1><span className="fa fa-sign-in"></span> Login</h1>
+        <div className="align-items-center">
+          <h1>
+            <span className="fa fa-sign-in" /> Login
+          </h1>
 
           <form>
-              <div className="form-group">
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </div>
 
-                  <input type="email" className="form-control" placeholder="Email" name="email"/>
-              </div>
-              <div className="form-group">
-
-                  <input type="password" className="form-control" placeholder="Password" name="password"/>
-              </div>
-
-              <button type="submit" className="btn btn-warning btn-lg">login</button>
+            <button type="submit" className="btn btn-warning btn-lg" onClick={this.handleFormSubmit}>
+              login
+            </button>
           </form>
 
-          <hr/>
+          <hr />
 
-          <p><a href="/register">Don't have an account? Sign up.</a></p>
-          <p><a href="/">Go back to home page.</a></p>
+          <p>
+            <a href="/register">Don't have an account? Sign up.</a>
+          </p>
+          <p>
+            <a href="/">Go back to home page.</a>
+          </p>
+        </div>
       </div>
-  </div>
     );
   }
 }
