88d5e9161ddf3b7b8d717d1f4eec77b553f36e1f
christinmd <christinadias@D-10-156-18-173.dhcp4.washington.edu>
2019-08-12 13:20:48 -0700

    pushing profile updates

--- src/components/ordercards/ordercards.js
+++ src/components/ordercards/ordercards.js
@@ -4,7 +4,7 @@ import "./ordercards.css";
 function OrderCard(props) {
     return (
 
-    <div className="container">
+    <div className="container order-container">
     <h3 className="order-name"><i class="random name"> </i> John Smith</h3>
 
         <div className="card-body">
