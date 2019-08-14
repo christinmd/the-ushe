daec5eefe6697e348bf0cb17019f5725442b7963
cfpaige <cfpaige@gmail.com>
2019-08-14 13:15:09 -0700

    fixes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,12 +9,11 @@ class Home extends Component {
   render() {
     return (
       <div>
-   
       <h1 className= "header-main" >The Ushe</h1>
       <h2 className= "header-home"> What's your Ushe?</h2>
       <h3 className= "header-second" > Share your favorite food and drink orders with the world. </h3>
-      <button type="button" class="btn btn-outline-primary"><a href="/login">Login</a></button>
-      <button type="button" class="btn btn-outline-primary"><a href="/register">Register</a></button>
+      <button type="button" className="btn btn-outline-primary"><a href="/login">Login</a></button>
+      <button type="button" className="btn btn-outline-primary"><a href="/register">Register</a></button>
       </div>
     );
   }
